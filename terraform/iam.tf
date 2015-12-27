cfba035af9e2d9809f60fb34124005d9c035433b
Nelson Elhage <nelhage@nelhage.com>
2015-12-26 20:55:28 -0500

    Import IAM roles

---
+++ terraform/iam.tf
@@ -0,0 +1,140 @@
+
+resource "aws_iam_role" "livegrep_frontend" {
+  name = "livegrep_frontend"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role" "livegrep_backend" {
+  name = "livegrep_backend"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "livegrep_s3" {
+    name = "livegrep-s3-ro"
+    path = "/"
+    description = "readonly access to the livegrep S3 bucket"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:HeadObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::livegrep/*",
+                "arn:aws:s3:::livegrep"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "livegrep_s3_attachment" {
+  name = "livegrep-s3-ro-attach"
+  roles = [
+    "${aws_iam_role.livegrep_frontend.name}",
+    "${aws_iam_role.livegrep_backend.name}",
+  ]
+  policy_arn = "${aws_iam_policy.livegrep_s3.arn}"
+}
+
+
+resource "aws_iam_role_policy" "livegrep_frontend_r53" {
+    name = "livegrep_frontend_r53"
+    role = "${aws_iam_role.livegrep_frontend.id}"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1450992162000",
+            "Effect": "Allow",
+            "Action": [
+                "route53:ListHostedZones"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Sid": "ManageIntZone",
+            "Effect": "Allow",
+            "Action": [
+                "route53:ListResourceRecordSets",
+                "route53:ChangeResourceRecordSets"
+            ],
+            "Resource": [
+                "arn:aws:route53:::hostedzone/Z3M7BVOL8R3KUV"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "livegrep_backend_r53" {
+    name = "livegrep_backend_r53"
+    role = "${aws_iam_role.livegrep_backend.id}"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1450992162000",
+            "Effect": "Allow",
+            "Action": [
+                "route53:ListHostedZones"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Sid": "ManageIntZone",
+            "Effect": "Allow",
+            "Action": [
+                "route53:ListResourceRecordSets",
+                "route53:ChangeResourceRecordSets"
+            ],
+            "Resource": [
+                "arn:aws:route53:::hostedzone/Z796FT2H4GG4"
+            ]
+        }
+    ]
+}
+EOF
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,94 @@
+
+resource "aws_vpc" "livegrep" {
+  cidr_block = "10.0.1.0/24"
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "livegrep" {
+  vpc_id = "${aws_vpc.livegrep.id}"
+  tags = {
+    Name = "livegrep"
+  }
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.livegrep.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.livegrep.id}"
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.livegrep.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "base" {
+  name        = "base"
+  description = "base security group"
+  vpc_id      = "${aws_vpc.livegrep.id}"
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "ICMP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ssh
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "livegrep_frontend" {
+  name        = "livegrep-frontend"
+  description = "livegrep frontend web server"
+  vpc_id      = "${aws_vpc.livegrep.id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9999
+    to_port     = 9999
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "livegrep_backend" {
+  name        = "livegrep-backend"
+  description = "livegrep backend index server"
+  vpc_id      = "${aws_vpc.livegrep.id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 8910
+    to_port     = 8910
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.livegrep_frontend.id}"]
+  }
+}
