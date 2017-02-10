fcda836fa17db11c2b456096b97a600738bc77cb
Nelson Elhage <nelhage@nelhage.com>
2017-02-09 20:30:53 -0800

    initial kubernetes import; can index linux

---
+++ gcloud/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -eux
+
+imgs=(base indexer)
+push=
+build=1
+
+TEMP=$(getopt -o '' --long push,build,no-build \
+     -n 'build.sh' -- "$@")
+
+if [ $? != 0 ] ; then exit 1 ; fi
+
+eval set -- "$TEMP"
+while true ; do
+    case "$1" in
+        --push) push=1; shift ;;
+        --build) build=1; shift ;;
+        --no-build) build=; shift ;;
+        --) shift; break ;;
+    esac
+done
+
+if [ "$#" -ne 0 ]; then
+    imgs=("$@")
+fi
+
+if [ "$build" ]; then
+    for img in "${imgs[@]}"; do
+        docker build -t "livegrep-$img" "$img"
+    done
+fi
+
+if [ "$push" ]; then
+    for img in "${imgs[@]}"; do
+        docker tag "livegrep-$img" "us.gcr.io/livegrep/$img"
+        gcloud docker -- push "us.gcr.io/livegrep/$img"
+    done
+fi
