3db7f776555bb1827b57b469ec62053090259c11
Rodrigo Silva Mendoza <rodrigo.silvamendoza@nytimes.com>
2021-10-09 20:16:12 -0700

    switch CI to listen for main branch instead of master

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 set -eux
 latest=
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_BRANCH" = "main" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     latest=--latest
 fi
 bin/build --push $latest
